1b93b4a43bb1856b361052dd3fc51c27aa7e3c81
Dipika Nikam <dipika.infynno@gmail.com>
2022-07-12 15:05:27 +0530

    check_hello

--- pytho1/hello_check.py
+++ pytho1/hello_check.py
@@ -3,9 +3,6 @@ string= 'hello how are you? all good hello'
 check = re.match('^hello',string)
 check2 = re.match('.* hello$',string)
 check3 = re.findall('hello',string)
-if string:
-    print(check)
-elif string:
-    print(check2)
-else:
-    print(check3)
+print(check)
+print(check2)
+print(check3)
